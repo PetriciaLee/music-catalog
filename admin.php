a651fb0cca1d6f008cedbd587e34b40abd51e916
Petra Jakopović <93216101+PetriciaLee@users.noreply.github.com>
2025-08-16 11:26:59 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+<title>IWA Projekt</title>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Projekt na temu Glazbeni katalog">
+<meta name="keywords" content="IWA, Projekt, Glazbeni katalog ">
+<meta name="author" content="Petra Jakopović">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stil.css" rel="stylesheet" type="text/css">
+ </head>
+  <body>
+ <div id="glavni">
+     
+<nav>
+ <div id="lijevo">
+   <a href="index.php"><b>IWA Projekti</b></a> 
+</div>  
+<a href="o_autoru.html">O autoru</a>
+<a href="medijskekuce.php">Medijske kuće</a>
+<a href="svikorisnici.php">Svi korisnici</a>  
+<a href='pjesme.php'>Pjesme</a> 
+<a href='index.php'>Početna</a> 
+    
+</nav>
+
+<div id="logo"><a name ="sidro"></a>
+</div>
+<br />
+
+<div id="sadrzaj">
+ <h1>Administrator</h1>
+  <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum a arcu cursus vitae congue mauris rhoncus aenean vel. Amet nisl suscipit adipiscing bibendum est ultricies. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Ullamcorper dignissim cras tincidunt lobortis. Nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Vitae aliquet nec ullamcorper sit amet risus. Pharetra massa massa ultricies mi quis hendrerit. Eu consequat ac felis donec et odio pellentesque. Vestibulum rhoncus est pellentesque elit. Ullamcorper sit amet risus nullam eget felis.</p>
+
+</div><br />
+ <footer><b> &copy Petra Jakopović 2024. | FOI </b></footer>   
+      </div>
+    </body>
+    </head>
+</html>
